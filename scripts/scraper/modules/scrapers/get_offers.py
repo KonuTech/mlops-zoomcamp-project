6b4df892a0289e98cf7420f36634dce1d804f09d
Konrad Borowiec <konradballegro@mlops-zoomcamp.europe-central2-a.c.ny-rides-konrad.internal>
2023-06-19 17:00:41 +0000

    scraper - update

--- scripts/scraper/modules/scrapers/get_offers.py
+++ scripts/scraper/modules/scrapers/get_offers.py
@@ -1,18 +1,16 @@
 import os
 import time
-from hashlib import sha1
 from concurrent.futures import ThreadPoolExecutor
+from hashlib import sha1
 
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+from utils.logger import console_logger, file_logger
 
-from utils.logger import console_logger
-from utils.logger import file_logger
-
-PATH_DATA = 'data'
-PATH_HEADER_FILE_PL = 'header_pl.txt'
-PATH_HEADER_FILE_EN = 'header_en.txt'
+PATH_DATA = "data"
+PATH_HEADER_FILE_PL = "header_pl.txt"
+PATH_HEADER_FILE_EN = "header_en.txt"
 MAX_THREADS = 8
 
 
@@ -29,11 +27,17 @@ class OfferScraper:
         path_data_directory=PATH_DATA,
         path_header_file_pl=PATH_HEADER_FILE_PL,
         path_header_file_en=PATH_HEADER_FILE_EN,
-        max_threads=MAX_THREADS
+        max_threads=MAX_THREADS,
     ):
-        self.path_data_directory = os.path.join(os.getcwd(), 'outputs', path_data_directory)
-        self.path_header_file_pl = os.path.join(os.getcwd(), 'inputs', path_header_file_pl)
-        self.path_header_file_en = os.path.join(os.getcwd(), 'inputs', path_header_file_en)
+        self.path_data_directory = os.path.join(
+            os.getcwd(), "outputs", path_data_directory
+        )
+        self.path_header_file_pl = os.path.join(
+            os.getcwd(), "inputs", path_header_file_pl
+        )
+        self.path_header_file_en = os.path.join(
+            os.getcwd(), "inputs", path_header_file_en
+        )
         self.max_threads = max_threads
         self.header_pl = self.get_header(self.path_header_file_pl)
         self.header_en = self.get_header(self.path_header_file_en)
@@ -45,7 +49,7 @@ class OfferScraper:
         :param header_file_path: path to the header file
         :return: a list of column names
         """
-        with open(header_file_path, 'r', encoding='utf-8') as file:
+        with open(header_file_path, "r", encoding="utf-8") as file:
             header = [x.strip() for x in file.readlines()]
 
         return header
@@ -66,46 +70,54 @@ class OfferScraper:
         :return:            a dictionary of offer's features
         """
         try:
-            file_logger.info(f'Fetching {url_path}')
+            file_logger.info(f"Fetching {url_path}")
 
             with requests.Session() as session:
                 response = session.get(url_path)
                 response.raise_for_status()
 
-            soup = BeautifulSoup(response.text, features='lxml')
+            soup = BeautifulSoup(response.text, features="lxml")
 
-            params = soup.find_all(class_='offer-params__item')
+            params = soup.find_all(class_="offer-params__item")
             batch = {
-                param.find('span', class_='offer-params__label').text.strip():
-                    param.find('div', class_='offer-params__value').text.strip() for param in params
+                param.find("span", class_="offer-params__label")
+                .text.strip(): param.find("div", class_="offer-params__value")
+                .text.strip()
+                for param in params
             }
 
-            values = soup.find_all('li', class_='parameter-feature-item')
+            values = soup.find_all("li", class_="parameter-feature-item")
             batch.update({value.text.strip(): 1 for value in values})
 
-            price = ''.join(soup.find('span', class_='offer-price__number').text.strip().split()[:-1])
-            batch['Cena'] = price
+            price = "".join(
+                soup.find("span", class_="offer-price__number")
+                .text.strip()
+                .split()[:-1]
+            )
+            batch["Cena"] = price
 
-            currency = soup.find('span', class_='offer-price__currency').text.strip()
-            batch['Waluta'] = currency
+            currency = soup.find("span", class_="offer-price__currency").text.strip()
+            batch["Waluta"] = currency
 
-            price_details = soup.find('span', class_='offer-price__details').text.strip()
-            batch['Szczegóły ceny'] = price_details
+            price_details = soup.find(
+                "span", class_="offer-price__details"
+            ).text.strip()
+            batch["Szczegóły ceny"] = price_details
 
-            batch['url_path'] = url_path
+            batch["url_path"] = url_path
 
-            batch['id'] = sha1(url_path.lower().encode('utf-8')).hexdigest()
+            batch["id"] = sha1(url_path.lower().encode("utf-8")).hexdigest()
 
             batch = self.new_line(main_features=batch)
 
-            batch['epoch'] = int(time.time())
+            batch["epoch"] = int(time.time())
 
             time.sleep(0.25)
 
             return batch
 
         except Exception as e:
-            file_logger.error(f'Error {e} while fetching {url_path}')
+            file_logger.error(f"Error {e} while fetching {url_path}")
 
     def get_offers(self, links: list) -> None:
         """
@@ -124,14 +136,17 @@ class OfferScraper:
         :param manufacturer:    car manufacturer name
         :return:                None
         """
-        file_logger.info(f'Saving {manufacturer} offers')
-        file_logger.info(f'Found {len(self.manufacturer)} offers')
-        console_logger.info(f'Found {len(self.manufacturer)} offers')
+        file_logger.info(f"Saving {manufacturer} offers")
+        file_logger.info(f"Found {len(self.manufacturer)} offers")
+        console_logger.info(f"Found {len(self.manufacturer)} offers")
 
         df = pd.DataFrame(self.manufacturer)
-        df.to_csv(os.path.join(self.path_data_directory, f'{manufacturer.strip()}.csv'), index=False)
+        df.to_csv(
+            os.path.join(self.path_data_directory, f"{manufacturer.strip()}.csv"),
+            index=False,
+        )
 
-        file_logger.info(f'Saved {manufacturer} offers')
+        file_logger.info(f"Saved {manufacturer} offers")
 
     def clear_list(self) -> None:
         """
--- scripts/scraper/modules/scrapers/offers_scraper.py
+++ scripts/scraper/modules/scrapers/offers_scraper.py
@@ -4,15 +4,13 @@ import time
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-
 from modules.scrapers.get_offers import OfferScraper
-from utils.logger import console_logger
-from utils.logger import file_logger
+from utils.logger import console_logger, file_logger
 
-PATH_DATA = 'data'
-PATH_MANUFACTURERS_FILE = 'manufacturers.txt'
-URL_BASE = 'https://www.otomoto.pl/osobowe/'
-OUTPUT_NAME = 'offers.csv'
+PATH_DATA = "data"
+PATH_MANUFACTURERS_FILE = "manufacturers.txt"
+URL_BASE = "https://www.otomoto.pl/osobowe/"
+OUTPUT_NAME = "offers.csv"
 
 
 class ManufacturerScraper:
@@ -23,12 +21,20 @@ class ManufacturerScraper:
         path_manufacturers_file: path to a file with names of manufacturers
     """
 
-    console_logger.info('Initializing a scraper')
-    file_logger.info('Initializing a scraper')
+    console_logger.info("Initializing a scraper")
+    file_logger.info("Initializing a scraper")
 
-    def __init__(self, path_data_directory=PATH_DATA, path_manufacturers_file=PATH_MANUFACTURERS_FILE):
-        self.path_manufacturers_file = os.path.join(os.getcwd(), 'inputs', path_manufacturers_file)
-        self.path_data_directory = os.path.join(os.getcwd(), 'outputs', path_data_directory)
+    def __init__(
+        self,
+        path_data_directory=PATH_DATA,
+        path_manufacturers_file=PATH_MANUFACTURERS_FILE,
+    ):
+        self.path_manufacturers_file = os.path.join(
+            os.getcwd(), "inputs", path_manufacturers_file
+        )
+        self.path_data_directory = os.path.join(
+            os.getcwd(), "outputs", path_data_directory
+        )
         self.manufacturers = self.get_manufacturers()
         self.offers = OfferScraper()
 
@@ -37,7 +43,7 @@ class ManufacturerScraper:
         Gets a list of manufacturers from a static file
         :return: a list of car manufacturers' names
         """
-        with open(self.path_manufacturers_file, 'r', encoding='utf-8') as file:
+        with open(self.path_manufacturers_file, "r", encoding="utf-8") as file:
             manufacturers = [line.strip() for line in file]
 
         return manufacturers
@@ -50,26 +56,29 @@ class ManufacturerScraper:
         :param i:       web page number
         :return:        a list of links
         """
-        console_logger.info(f'Scraping page: {i}')
-        file_logger.info(f'Scraping page: {i}')
+        console_logger.info(f"Scraping page: {i}")
+        file_logger.info(f"Scraping page: {i}")
 
         with requests.Session() as session:
-            response = session.get(f'{path}?page={i}')
+            response = session.get(f"{path}?page={i}")
             response.raise_for_status()
 
-        soup = BeautifulSoup(response.text, features='lxml')
+        soup = BeautifulSoup(response.text, features="lxml")
 
-        car_links_section = soup.find('main', attrs={'data-testid': 'search-results'})
+        car_links_section = soup.find("main", attrs={"data-testid": "search-results"})
 
         if car_links_section is None:
-            console_logger.warning(f'No car links found on page {i}')
-            file_logger.warning(f'No car links found on page {i}')
+            console_logger.warning(f"No car links found on page {i}")
+            file_logger.warning(f"No car links found on page {i}")
             return []
 
-        links = [x.find('a', href=True)['href'] for x in car_links_section.find_all('article')]
+        links = [
+            x.find("a", href=True)["href"]
+            for x in car_links_section.find_all("article")
+        ]
 
-        console_logger.info(f'Found {len(links)} links')
-        file_logger.info(f'Found {len(links)} links')
+        console_logger.info(f"Found {len(links)} links")
+        file_logger.info(f"Found {len(links)} links")
 
         return links
 
@@ -81,30 +90,34 @@ class ManufacturerScraper:
         """
         manufacturer = manufacturer.strip()
 
-        console_logger.info(f'Start of scraping the manufacturer: {manufacturer}')
-        file_logger.info(f'Start of scraping the manufacturer: {manufacturer}')
+        console_logger.info(f"Start of scraping the manufacturer: {manufacturer}")
+        file_logger.info(f"Start of scraping the manufacturer: {manufacturer}")
 
         # Clear the list of offers
         self.offers.clear_list()
 
-        url = f'{URL_BASE}{manufacturer}'
+        url = f"{URL_BASE}{manufacturer}"
 
         try:
             with requests.Session() as session:
                 response = session.get(url)
                 response.raise_for_status()
 
-            soup = BeautifulSoup(response.text, features='lxml')
-            last_page_num = int(soup.find_all('li', attrs={'data-testid': 'pagination-list-item'})[-1].text)
+            soup = BeautifulSoup(response.text, features="lxml")
+            last_page_num = int(
+                soup.find_all("li", attrs={"data-testid": "pagination-list-item"})[
+                    -1
+                ].text
+            )
 
         except Exception as e:
-            file_logger.error(f'Error {e} while searching for last_page_num')
+            file_logger.error(f"Error {e} while searching for last_page_num")
             last_page_num = 1
 
         last_page_num = min(last_page_num, 1000)
 
-        console_logger.info(f'Manufacturer has: {last_page_num} subpages')
-        file_logger.info(f'Manufacturer has: {last_page_num} subpages')
+        console_logger.info(f"Manufacturer has: {last_page_num} subpages")
+        file_logger.info(f"Manufacturer has: {last_page_num} subpages")
 
         pages = range(1, last_page_num + 1)
 
@@ -117,22 +130,22 @@ class ManufacturerScraper:
         # Save the list of offers
         self.offers.save_offers(manufacturer=manufacturer)
 
-        console_logger.info(f'End of scraping the manufacturer: {manufacturer}')
-        file_logger.info(f'End of scraping the manufacturer: {manufacturer}')
+        console_logger.info(f"End of scraping the manufacturer: {manufacturer}")
+        file_logger.info(f"End of scraping the manufacturer: {manufacturer}")
 
     def scrap_all_manufacturers(self) -> None:
         """
         Loops over the list of manufacturer names to scrape data for each one of them
         :return: None
         """
-        console_logger.info('Starting scraping cars...')
-        file_logger.info('Starting scraping cars...')
+        console_logger.info("Starting scraping cars...")
+        file_logger.info("Starting scraping cars...")
 
         for m, manufacturer in enumerate(self.manufacturers):
             self.scrap_manufacturer(manufacturer=manufacturer)
 
-        console_logger.info('End of scraping manufacturers')
-        file_logger.info('End of scraping manufacturers')
+        console_logger.info("End of scraping manufacturers")
+        file_logger.info("End of scraping manufacturers")
 
     def dump_data(self) -> None:
         """
@@ -140,14 +153,12 @@ class ManufacturerScraper:
         :return: None
         """
 
-        console_logger.info('Appending the data...')
-        file_logger.info('Appending the data...')
-
-        # Update self.header_en
-        # self.offers.header_en = self.get_header_en()
+        console_logger.info("Appending the data...")
+        file_logger.info("Appending the data...")
 
         filenames = [
-            os.path.join(self.path_data_directory, f'{manufacturer.strip()}.csv') for manufacturer in self.manufacturers
+            os.path.join(self.path_data_directory, f"{manufacturer.strip()}.csv")
+            for manufacturer in self.manufacturers
         ]
 
         combined_data = []
@@ -159,10 +170,14 @@ class ManufacturerScraper:
                 combined_data.append(data)
 
             except Exception as e:
-                file_logger.error(f'Error {e} while searching for {filename}')
+                file_logger.error(f"Error {e} while searching for {filename}")
 
         df = pd.concat(combined_data, ignore_index=True)
-        df.to_csv(os.path.join(self.path_data_directory, OUTPUT_NAME), index=False, encoding='utf-8')
+        df.to_csv(
+            os.path.join(self.path_data_directory, OUTPUT_NAME),
+            index=False,
+            encoding="utf-8",
+        )
 
-        console_logger.info(f'Appended data saved as {OUTPUT_NAME}')
-        file_logger.info(f'Appended data saved as {OUTPUT_NAME}')
+        console_logger.info(f"Appended data saved as {OUTPUT_NAME}")
+        file_logger.info(f"Appended data saved as {OUTPUT_NAME}")
